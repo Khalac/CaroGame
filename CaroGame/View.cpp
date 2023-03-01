f736c1177e8404f541f4f2d719ac6499fb775c78
VÂN MAI ĐỨC <22127449@student.hcmus.edu.vn>
2023-03-01 23:26:40 +0700

    test

--- CaroGame/View.cpp
+++ CaroGame/View.cpp
@@ -22,3 +22,22 @@ _POINT _A[BOARD_SIZE][BOARD_SIZE]; //Ma trận bàn cờ
 bool _TURN; //true là lượt người thứ nhất và false là lượt người thứ hai
 int _COMMAND; // Biến nhận giá trị phím người dùng nhập
 int _X, _Y; //Tọa độ hiện hành trên màn hình bàn cờ
+void ResetData() {
+	for (int i = 0; i < BOARD_SIZE; i++) {
+		for (int j = 0; j < BOARD_SIZE; j++) {
+			_A[i][j].x = 4 * j + LEFT + 2; // Trùng với hoành độ màn hình bàn cờ
+			_A[i][j].y = 2 * i + TOP + 1; // Trùng với tung độ màn hình bàn cờ
+			_A[i][j].c = 0; // 0 nghĩa là chưa ai đánh dấu, nếu đánh dấu phải theo quy
+			//định như sau: -1 là lượt true đánh, 1 là lượt false đánh
+		}
+	}
+	_TURN = true; _COMMAND = -1; // Gán lượt và phím mặc định _X = _A[0][0].x; _Y = _A[0][0].y;
+}
+void DrawBoard(int pSize) {
+		for (int i = 0; i <= pSize; i++) {
+				for (int j = 0; j <= pSize; j++) {
+					GotoXY(LEFT + 4 * i, TOP + 2 * j);
+					printf(".");
+				}
+		}
+}
