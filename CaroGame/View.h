5241907278e1dbbb8c53296c5116e2240c186828
FPT136 <FPT136@DESKTOP-G9815ER>
2023-03-06 23:49:41 +0700

    Ve bang ben canh ban co

--- CaroGame/View.h
+++ CaroGame/View.h
@@ -1,9 +1,12 @@
 ﻿#pragma once
 void FixConsoleWindow();
 void GotoXY(int x, int y);
+void gotoxy(int x, int y);//gotoxy fake
 struct _POINT { int x, y, c; }; // x: tọa độ dòng, y: tọa độ cột, c: đánh dấu
 void ResetData();
 void StartGame();
+void draw();
 void TextColor(int color);
 void UnNocursortype();
 void Nocursortype();
+void bangtinhdiem();//Bang ben canh ban co
