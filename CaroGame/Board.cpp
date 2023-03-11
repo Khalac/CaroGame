2149604f979e4f54c711115deb3513f4e5db24c0
vominh <vominh272004@gmail.com>
2023-03-11 22:48:45 +0700

    Chỉnh lại 13x13

--- CaroGame/Board.cpp
+++ CaroGame/Board.cpp
@@ -13,9 +13,9 @@ void draw()
 
     for (int i = 0; i <= 27; i++)
     {
-        for (int j = 0; j <= 17; j++)
+        for (int j = 0; j <= 16; j++)
         {
-            if (i < 1)//tao khaong cach ben tren
+            if (i < 1)//tao khoang cach ben tren
             {
                 printf(" ");
             }
@@ -23,40 +23,40 @@ void draw()
             {
                 gotoxy(1, 1);
                 printf("%c", 218);
-                gotoxy(57, 1);
+                gotoxy(53, 1);//57
                 printf("%c", 191);
-                for (int k = 2; k < 57; k++)
+                for (int k = 2; k < 53; k++)
                 {
                     gotoxy(k, 1);
                     printf("%c", 196);
 
                 }
-                for (int k = 5; k < 57; )
+                for (int k = 5; k < 53; )
                 {
                     gotoxy(k, 1);
                     printf("%c", 194);
                     k += 4;
 
                 }
-            }//
+            }
 
             else if (j < 1)//Tao khoang cach ben trai
             {
                 printf(" ");
             }
-            else if (i % 2 == 1 && j > 1 && j < 16 && i>1) {
+            else if (i % 2 == 1 && j > 1 && j < 15 && i>1) {
                 printf("%c%c%c%c", 197, 196, 196, 196);
 
             }
-            else if (i % 2 == 0 && j > 1 && j < 16 && i>1)
+            else if (i % 2 == 0 && j > 1 && j < 15 && i>1)
             {
                 printf("%c   ", 179);
 
             }
 
-            else if (i % 2 == 1 && j == 16 && i > 1 && j > 2 && i < 27)
+            else if (i % 2 == 1 && j == 15 && i > 1 && j > 2 && i < 27)
                 printf("%c", 197);
-            else if (i % 2 == 0 && j == 16 && i > 1 && i < 27)
+            else if (i % 2 == 0 && j == 15 && i > 1 && i < 27)
                 printf("%c", 179);
 
         }
@@ -71,36 +71,34 @@ void draw()
             printf("%c", 195);
         }
     }
-    gotoxy(57, 27);// ve vien ben phai
+    gotoxy(53, 27);// ve vien ben phai
     printf("%c", 217);
     for (int m = 3; m < 27; m++)
     {
         if (m % 2 == 1)
         {
-            gotoxy(57, m);
+            gotoxy(53, m);
             printf("%c", 180);
         }
     }
     gotoxy(1, 27);//ve vien ben duoi
     printf("%c", 192);
-    for (int k = 2; k < 57; k++)
+    for (int k = 2; k < 53; k++)
     {
         gotoxy(k, 27);
         printf("%c", 196);
 
     }
-    for (int m = 5; m < 57; )
+    for (int m = 5; m < 53; )
     {
         gotoxy(m, 27);
         printf("%c", 193);
         m += 4;
     }
-    gotoxy(54, 27);
+    gotoxy(51, 27);
     printf("%c", 196);
-    gotoxy(55, 27);
+    gotoxy(52, 27);
     printf("%c", 196);
-    gotoxy(56, 27);
-    printf("%c", 196);
-    gotoxy(57, 27);// ve vien ben phai
+    gotoxy(53, 27);
     printf("%c", 217);
 }
